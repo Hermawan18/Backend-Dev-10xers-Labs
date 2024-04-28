93cb8f33bff840527e30512fc1e36ac24587ccbe
Hermawan18 <hermawan18.hty@gmail.com>
2024-04-28 16:47:02 +0700

    feat: authentication

---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+const { verifyToken } = require('../helpers/jwt');
+
+function authentication(req, res, next) {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw { name: 'Unauthorized' };
+    }
+
+    const token = authorization.split(' ')[1];
+    const decode = verifyToken(token);
+
+    if (decode.role !== 'admin') {
+      throw { name: 'Unauthorized' };
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = authentication;
