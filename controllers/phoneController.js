6224dd4248cea604d001c8dcea00c730c8f9fcd2
Hermawan18 <hermawan18.hty@gmail.com>
2024-04-28 18:49:21 +0700

    feat: update phone

--- controllers/phoneController.js
+++ controllers/phoneController.js
@@ -1,7 +1,7 @@
 const { Phone } = require('../models');
 
 class phoneController {
-  static async getAllPhones(req, res) {
+  static async getAllPhones(req, res, next) {
     try {
       const phones = await Phone.findAll();
       res.status(200).json(phones);
@@ -10,7 +10,7 @@ class phoneController {
     }
   }
 
-  static async createPhone(req, res) {
+  static async createPhone(req, res, next) {
     try {
       const userId = req.user.id;
       const { brand, model, colors, price, stock } = req.body;
@@ -18,6 +18,19 @@ class phoneController {
       const newPhone = await Phone.create({ brand, model, colors, price, stock, userId });
 
       res.status(201).json(newPhone);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async updatePhone(req, res, next) {
+    try {
+      const { brand, model, colors, price, stock } = req.body;
+      const { id } = req.params;
+
+      const phone = await Phone.update({ brand, model, colors, price, stock }, { where: { id }, returning: true });
+
+      res.status(200).json(phone[1]);
     } catch (error) {
       next(error);
     }
