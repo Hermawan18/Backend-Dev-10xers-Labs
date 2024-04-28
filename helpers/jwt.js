ad46058ca657383249aeac2558c86ed9f9fd7646
Hermawan18 <hermawan18.hty@gmail.com>
2024-04-28 20:37:51 +0700

    doc: api

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET || 'secret';
 
 const signToken = (payload) => jwt.sign(payload, secret);
 const verifyToken = (token) => jwt.verify(token, secret);
